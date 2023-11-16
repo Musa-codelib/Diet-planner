39a9e7b3bff1a9c7b70e46d0dc16c80b5a8f5a57
mkodes <136096862+Musa-codelib@users.noreply.github.com>
2023-11-16 17:10:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ function addMeal() {
 
     // You can add further logic here, such as storing the data in a database or localStorage.
 }
+
+function calculateBMI() {
+    var feet = document.getElementById("feet").value;
+    var inches = document.getElementById("inches").value;
+    var weight = document.getElementById("weight").value;
+
+    if (feet && inches && weight) {
+        var heightInMeters = ((feet * 12) + parseInt(inches)) * 0.0254; // Convert height to meters
+        var bmi = (weight / (heightInMeters * heightInMeters)).toFixed(2);
+        var bmiResult = document.getElementById("bmiResult");
+        bmiResult.textContent = `Your BMI is: ${bmi} - ${getBMICategory(bmi)}`;
+
+        // You can add further logic here, such as providing BMI categories (underweight, normal weight, overweight, etc.).
+    } else {
+        alert("Please enter both height (feet and inches) and weight to calculate BMI.");
+    }
+}
+
+function getBMICategory(bmi) {
+    if (bmi < 18.5) {
+        return "Underweight";
+    } else if (bmi >= 18.5 && bmi < 25) {
+        return "Normal Weight";
+    } else if (bmi >= 25 && bmi < 30) {
+        return "Overweight";
+    } else {
+        return "Obese";
+    }
+}
